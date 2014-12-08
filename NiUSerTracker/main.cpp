183ed0e4582a40dfb0e443a836dbd19667e70ab6
Mikael <mikael@wificampus-236053.local.univ-savoie.fr>
2014-12-08 09:03:53 +0100

    Initial Commit

---
+++ NiUSerTracker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NiUSerTracker
+//
+//  Created by Mikael on 08/12/2014.
+//  Copyright (c) 2014 mikael. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
